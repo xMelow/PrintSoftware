5e0e0f597e0f8da9d68fcf84b1cf60f976943c30
Flor Stellamans <flor@altec.be>
2025-11-14 15:34:01 +0100

    add test folder

---
+++ Tests/Services/LabelServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using PrintSoftware.Domain.Label;
+using PrintSoftware.Services;
+
+namespace Tests
+{
+    [TestClass]
+    public sealed class LabelServiceTest
+    {
+        private Label testLabel;
+        private LabelService labelService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            testLabel = new Label("test label", 110, 110, "mm");
+            labelService = new LabelService();
+        }
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
