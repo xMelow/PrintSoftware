f217ef76fb14b8f1f010e23939f262e23fbc284e
Flor Stellamans <flor@altec.be>
2025-11-17 08:36:09 +0100

    add update test folder

--- Tests/Services/LabelPreviewServiceTest.cs
+++ Tests/Services/LabelPreviewServiceTest.cs
@@ -1,6 +1,44 @@
-﻿namespace Tests;
+﻿using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrintSoftware.Domain.Label;
+using PrintSoftware.Domain.Label.LabelElements;
+using PrintSoftware.Services;
 
+namespace Tests.Services;
+
+[TestClass]
+[TestSubject(typeof(LabelPreviewService))]
 public class LabelPreviewServiceTest
 {
+    private Label _testLabel;
+    private LabelPreviewService _labelPreviewService;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _testLabel = new Label("test label", 110, 110, "mm");
+        _testLabel.LabelElements = new List<LabelElement>()
+        {
+            new TextElement("title", 427, 47, "", "0", 20),
+            new TextElement(77, 234, "0", 0, 12, "Name: "), 
+            new TextElement("name", 295, 231, "Flor Stellamans", "0", 16),
+            new CircleElement(933, 590, 260, 12), 
+            new BoxElement(33, 178, 1199, 7, 6)
+        };
+        _labelPreviewService = new LabelPreviewService(_testLabel);
+    }
+
+    [TestMethod]
+    public void RenderStaticLabelPreview()
+    {
+        var result = _labelPreviewService.RenderStaticLabelPreview();
+        Assert.IsNotNull(result);
+    }
     
+    [TestMethod]
+    public void RenderDynamicLabelPreview()
+    {
+        var result = _labelPreviewService.RenderDynamicLabelElements();
+        Assert.IsNotNull(result);
+    }
 }
--- Tests/Services/LabelServiceTest.cs
+++ Tests/Services/LabelServiceTest.cs
@@ -1,4 +1,5 @@
 ﻿using PrintSoftware.Domain.Label;
+using PrintSoftware.Domain.Label.LabelElements;
 using PrintSoftware.Services;
 
 namespace Tests
@@ -6,19 +7,34 @@ namespace Tests
     [TestClass]
     public sealed class LabelServiceTest
     {
-        private Label testLabel;
-        private LabelService labelService;
+        private Label _testLabel;
+        private LabelService _labelService;
 
         [TestInitialize]
-        public void Initialize()
+        public void TestInitialize()
         {
-            testLabel = new Label("test label", 110, 110, "mm");
-            labelService = new LabelService();
+            _testLabel = new Label("test label", 110, 110, "mm");
+            _testLabel.LabelElements = new List<LabelElement>()
+            {
+                new TextElement("title", 427, 47, "", "0", 20),
+                new TextElement(77, 234, "0", 0, 12, "Name: "), 
+                new TextElement("name", 295, 231, "Flor Stellamans", "0", 16),
+                new CircleElement(933, 590, 260, 12), 
+                new BoxElement(33, 178, 1199, 7, 6)
+            };
+            _labelService = new LabelService();
         }
-        
+
         [TestMethod]
-        public void TestMethod1()
+        public void UpdateLabelElementDataTest()
         {
+            _labelService.UpdateLabelDataElement("title", "This is the title");
+            
+            var textElement = _testLabel.LabelElements
+                .OfType<TextElement>()
+                .FirstOrDefault(e => e.Name == "title");
+            
+            Assert.AreEqual("This is the title", textElement.Content);
         }
     }
 }
