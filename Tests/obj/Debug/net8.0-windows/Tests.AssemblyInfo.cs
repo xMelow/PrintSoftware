31ca7d02752ee2cd7f4a013c6315c2b8a6977848
Flor Stellamans <flor@altec.be>
2025-11-17 09:16:24 +0100

    add all file changes

--- Tests/obj/Debug/net8.0-windows/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0-windows/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19693ae68e33b315729b1d3303b5f792a08052f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f217ef76fb14b8f1f010e23939f262e23fbc284e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
